c89de19e24a276a8934b18c9644756ed36e634f4
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-22 11:55:35 +0000

    Make some progress

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const input = document.querySelector('input')
-const paragraph = document.querySelector('p')
-const container = document.querySelector('.container')
-const arr = []
-const div = document.createElement('div')
-div.style.backgroundColor = 'red'
+// const input = document.querySelector('input')
+// const paragraph = document.querySelector('p')
+// const container = document.querySelector('.container')
+// const arr = []
+// const div = document.createElement('div')
+// div.style.backgroundColor = 'red'
 
-input.addEventListener('input', () => {
-    paragraph.innerText = input.value
-})
+// input.addEventListener('input', () => {
+//     paragraph.innerText = input.value
+// })
 
-for (let i = 0; i < input.value; i++) {
-    arr.push('<div></div>')  
-}
-console.log(arr)
-container.innerHTML = arr
+// for (let i = 0; i < input.value; i++) {
+//     arr.push('<div></div>')  
+// }
+// console.log(arr)
+// container.innerHTML = arr
