5290fd803acc9acb3b449aa6b144030f78bf3de2
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-23 19:10:44 +0000

    Create grid according to input.value and inner div

--- main.js
+++ main.js
@@ -3,23 +3,69 @@ const paragraph = document.querySelector('p')
 const container = document.querySelector('.container')
 const arr = []
 const div = document.createElement('div')
-div.style.backgroundColor = 'red'
 
-input.addEventListener('input', () => {
-    container.style.gridTemplateColumns =  `repeat(16, 1fr)`
-    container.style.gridTemplateRows =  `repeat(16, 1fr)`
-    container.innerHTML = ''
-    paragraph.innerText = input.value
-    for (let i = 0; i < input.value; i++) {
-        arr.push('<div></div>')  
-        container.innerHTML += arr[i]
-    }
-})
+container.style.gridTemplateColumns =  `repeat(${input.value}, 1fr)`
+container.style.gridTemplateRows =  `repeat(${input.value}, 1fr)`
+for(let i =0; i < Math.pow(input.value, 2); i++){
+    const div = document.createElement('div')
+    div.style.backgroundColor = 'blue'
+    container.append(div)
+}
 
+paragraph.innerText = input.value
 
+input.addEventListener('input', main)
 
 
-console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// div.style.backgroundColor = 'red'
+
+// input.addEventListener('input', () => {
+//     container.style.gridTemplateColumns =  `repeat(${input.value}, 1fr)`
+//     container.style.gridTemplateRows =  `repeat(${input.value}, 1fr)`
+//     container.innerHTML = ''
+//     paragraph.innerText = input.value
+//     for (let i = 0; i < Math.pow(input.value, 2); i++) {
+//         arr.push('<div></div>')  
+//         container.innerHTML += arr[i]
+//     }
+// })
+
+
+
+
+// console.log(arr)
 
 
 // style.setProperty('grid-template-columns', 'repeat(' + variable + ', 1fr)');
